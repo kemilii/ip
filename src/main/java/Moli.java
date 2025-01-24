14b40e4f9f83738df7eae64efeb2d934580e5c0b
Jiali <79980461+kemilii@users.noreply.github.com>
2025-01-24 13:09:42 +0800

    Level-0: Rename, Greet, Exit

---
+++ src/main/java/Moli.java
@@ -0,0 +1,18 @@
+public class Moli {
+    public static void main(String[] args) {
+        String divider = "——————————————————————————————————";
+        String logo = "\n"+
+                "                ███╗   ███╗ ██████╗ ██╗     ██╗\n" +
+                "                ████╗ ████║██╔═══██╗██║     ██║\n" +
+                "                ██╔████╔██║██║   ██║██║     ██║\n" +
+                "                ██║╚██╔╝██║██║   ██║██║     ██║\n" +
+                "                ██║ ╚═╝ ██║╚██████╔╝███████╗██║\n" +
+                "                ╚═╝     ╚═╝ ╚═════╝ ╚══════╝╚═╝";
+        System.out.println(logo);
+        System.out.println(divider);
+        System.out.println("Hello! I'm Moli.\nWhat can I do for you?");
+        System.out.println(divider);
+        System.out.println("Bye. Hope to see you soon.");
+        System.out.println(divider);
+    }
+}
