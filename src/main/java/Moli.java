4b55fb482e6286ab21dbaaebcd0ee50a43899389
Jiali <79980461+kemilii@users.noreply.github.com>
2025-01-30 23:11:35 +0800

    Level-2: Add, List

--- src/main/java/Moli.java
+++ src/main/java/Moli.java
@@ -1,9 +1,16 @@
 import java.util.Scanner;
 
 public class Moli {
+    private static final int MAX_TASKS = 100;
+    private static final String[] tasks = new String[MAX_TASKS];
+    private static int taskCount = 0;
 
+    final static String DIVIDER = "____________________________________________________________";
+
+    /**
+     * Main method that starts the chatbot.
+     */
     public static void main(String[] args) {
-        final String DIVIDER = "____________________________________________________________";
         final String LOGO = "\n"
                 + "                ███╗   ███╗ ██████╗ ██╗     ██╗\n"
                 + "                ████╗ ████║██╔═══██╗██║     ██║\n"
@@ -27,14 +34,48 @@ public class Moli {
                 System.out.println("Take care of yourself💖\nCome back anytime you need a listening ear.");
                 System.out.println(DIVIDER);
                 quit = true;
-            }
-            else{
-                System.out.println(DIVIDER);
-                System.out.println(" You said: " + input);
-                System.out.println(" " + "I'm listening💖, tell me more.");
-                System.out.println(DIVIDER);
+            }else if ("list".equalsIgnoreCase(input)) {
+                displayTasks();
+            } else {
+                addTask(input);
             }
         }
         scanner.close();
+    }
+
+    /**
+     * Adds a task to the task list.
+     *
+     * @param task    The task to be added.
+     */
+    private static void addTask(String task) {
+        if (taskCount < MAX_TASKS) {
+            tasks[taskCount] = task;
+            taskCount++;
+            System.out.println(DIVIDER);
+            System.out.println("Added: " + task + " ✅");
+            System.out.println("I'm listening, tell me more.");
+            System.out.println(DIVIDER);
+        } else {
+            System.out.println(DIVIDER);
+            System.out.println("Oh no! Sorry I can only remember up to " + MAX_TASKS + " tasks:(");
+            System.out.println(DIVIDER);
+        }
+    }
+
+    /**
+     * Displays the list of stored tasks.
+     */
+    private static void displayTasks() {
+        System.out.println(DIVIDER);
+        if (taskCount == 0) {
+            System.out.println("Your task list is empty.\n What would you like to do? I'm always happy to help you record.");
+        } else {
+            System.out.println("Here’s what you’ve told me so far: ");
+            for (int i = 0; i < taskCount; i++) {
+                System.out.println((i + 1) + ". " + tasks[i]);
+            }
+        }
+        System.out.println(DIVIDER);
     }
 }
