2cf677b97c0090cc2718603181eb59a317455cb1
Jiali <79980461+kemilii@users.noreply.github.com>
2025-01-30 22:52:59 +0800

    Level-1: Echo

--- src/main/java/Moli.java
+++ src/main/java/Moli.java
@@ -1,18 +1,40 @@
+import java.util.Scanner;
+
 public class Moli {
+
     public static void main(String[] args) {
-        String divider = "——————————————————————————————————";
-        String logo = "\n"+
-                "                ███╗   ███╗ ██████╗ ██╗     ██╗\n" +
-                "                ████╗ ████║██╔═══██╗██║     ██║\n" +
-                "                ██╔████╔██║██║   ██║██║     ██║\n" +
-                "                ██║╚██╔╝██║██║   ██║██║     ██║\n" +
-                "                ██║ ╚═╝ ██║╚██████╔╝███████╗██║\n" +
-                "                ╚═╝     ╚═╝ ╚═════╝ ╚══════╝╚═╝";
-        System.out.println(logo);
-        System.out.println(divider);
-        System.out.println("Hello! I'm Moli.\nWhat can I do for you?");
-        System.out.println(divider);
-        System.out.println("Bye. Hope to see you soon.");
-        System.out.println(divider);
+        final String DIVIDER = "____________________________________________________________";
+        final String LOGO = "\n"
+                + "                ███╗   ███╗ ██████╗ ██╗     ██╗\n"
+                + "                ████╗ ████║██╔═══██╗██║     ██║\n"
+                + "                ██╔████╔██║██║   ██║██║     ██║\n"
+                + "                ██║╚██╔╝██║██║   ██║██║     ██║\n"
+                + "                ██║ ╚═╝ ██║╚██████╔╝███████╗██║\n"
+                + "                ╚═╝     ╚═╝ ╚═════╝ ╚══════╝╚═╝";
+
+        System.out.println(LOGO);
+        System.out.println(DIVIDER);
+        System.out.println("Hello, dear one. I'm Moli.\nI'm here for you💙");
+        System.out.println(DIVIDER);
+
+        Scanner scanner = new Scanner(System.in);
+        boolean quit = false;
+        while (!quit) {
+            String input = scanner.nextLine().trim();
+
+            if ("bye".equalsIgnoreCase(input)) {
+                System.out.println(DIVIDER);
+                System.out.println("Take care of yourself💖\nCome back anytime you need a listening ear.");
+                System.out.println(DIVIDER);
+                quit = true;
+            }
+            else{
+                System.out.println(DIVIDER);
+                System.out.println(" You said: " + input);
+                System.out.println(" " + "I'm listening💖, tell me more.");
+                System.out.println(DIVIDER);
+            }
+        }
+        scanner.close();
     }
 }
